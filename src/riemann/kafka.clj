62b5388b389b48f55bf2e8f65dfb8a7f6fc0967a
Kamil Toman <kamil.toman@leafclick.com>
2023-09-19 11:14:32 +0200

    Use custom kafka client instead of kinsky consumer.

--- src/riemann/kafka.clj
+++ src/riemann/kafka.clj
@@ -6,7 +6,12 @@
   (:use [riemann.common        :only [event]]
         [riemann.core          :only [stream!]]
         [riemann.service       :only [Service ServiceEquiv ServiceStatus]]
-        [clojure.tools.logging :only [debug info error]]))
+        [clojure.tools.logging :only [debug info error]])
+  (:import (org.apache.kafka.clients.consumer KafkaConsumer ConsumerRecord)
+           (java.util Collection Map)
+           (org.apache.kafka.common.serialization Deserializer)
+           (java.time Duration)
+           (clojure.lang ILookup)))
 
 (defn kafka
   "Returns a function that is invoked with a topic name and an optional message key and returns a stream. That stream is a function which takes an event or a sequence of events and sends them to Kafka.
@@ -55,52 +60,59 @@
       (when payload
         (try
           (json/read-json (String. payload "UTF-8") :key-fn keyword)
-        (catch Exception e
-          (error e "Could not decode message")))))))
+          (catch Exception e
+            (error e "Could not decode message")))))))
+
+(defprotocol Runner
+  (is-running?
+    [this]))
 
 (defn start-kafka-thread
   "Start a kafka thread which will pop messages off the queue as long
   as running? is true"
   [running? core opts]
-  (let [opts (merge {:consumer.config {:bootstrap.servers "localhost:9092"
-                                       :group.id "riemann"}
-                     :topics ["riemann"]
-                     :key.deserializer client/keyword-deserializer
-                     :value.deserializer json-deserializer
-                     :poll.timeout.ms 100}
+  (let [opts (merge {:consumer.config    {:bootstrap.servers "localhost:9092"
+                                          :group.id          "riemann"}
+                     :topics             ["riemann"]
+                     :key.deserializer   (client/keyword-deserializer)
+                     :value.deserializer (json-deserializer)
+                     :poll.timeout.ms    100}
                     opts)
-        consumer (client/consumer (dissoc (:consumer.config opts) :enable.auto.commit)
-                                  (:key.deserializer opts)
-                                  (:value.deserializer opts))
-        topics (flatten (:topics opts))]
+        ^KafkaConsumer consumer (KafkaConsumer. ^Map (client/opts->props (dissoc (:consumer.config opts) :enable.auto.commit))
+                                                ^Deserializer (:key.deserializer opts)
+                                                ^Deserializer (:value.deserializer opts)
+                                                )
+        ^Collection topics (flatten (:topics opts))
+        ^Duration timeoutMs (Duration/ofMillis (:poll.timeout.ms opts))]
     (future
       (try
         (info "Subscribing to " topics "...")
-        (client/subscribe! consumer topics)
+        (.subscribe consumer topics)
         (while @running?
-          (let [msgs (client/poll! consumer (:poll.timeout.ms opts))
-                msgs-by-topic (get msgs :by-topic)]
-            (doseq [records msgs-by-topic
-                    record (last records)]
-              (try
-                (let [event (event (get record :value))]
-                  (stream! @core event))
-                (catch java.lang.NullPointerException e
-                  (debug (str "Invalid message: " record)))))))
+          (let [consumerRecords (.poll consumer timeoutMs)]
+            (doseq [^ConsumerRecord record (iterator-seq (.iterator consumerRecords))]
+              (let [value (.value record)]
+                (if (map? value)
+                  (stream! @core value)
+                  (doseq [v value] (stream! @core v)))
+                )
+              )
+            )
+          )
         (catch Exception e
           (do
             (reset! running? false)
             (error e "Interrupted consumption")))
         (finally
-          (client/close! consumer))))))
+          (.close consumer))))))
 
 (defn kafka-consumer
   "Yield a kafka consumption service"
   [opts]
   (let [running? (atom true)
-        core     (atom nil)]
+        core (atom nil)]
     (reify
-      clojure.lang.ILookup
+      ILookup
       (valAt [this k not-found]
         (or (.valAt this k) not-found))
       (valAt [this k]
@@ -109,18 +121,19 @@
       ServiceEquiv
       (equiv? [this other]
         (= opts (:opts other)))
-      ServiceStatus
-      (running? [this] @running?)
       Service
       (conflict? [this other]
         (= opts (:opts other)))
       (start! [this]
         (when-not test/*testing*
-          (do (info "Starting kafka consumer")
-              (start-kafka-thread running? core opts))))
+          (info "Starting kafka consumer")
+          (start-kafka-thread running? core opts)))
       (reload! [this new-core]
         (info "Reload called, setting new core value")
         (reset! core new-core))
       (stop! [this]
         (reset! running? false)
-        (info "Stopping kafka consumer")))))
+        (info "Stopping kafka consumer"))
+      Runner
+      (is-running? [this]
+        @running?))))
